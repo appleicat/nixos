1c2f2e94cc4dd2c9e20283ee7e2ea3f7c870b243
kitty <87716149+appleicat@users.noreply.github.com>
2024-03-04 23:14:41 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "NixOS configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      default = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
