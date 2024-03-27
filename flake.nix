e7217b175c1e42ba52af232972c2b296fe39a4af
kitty <87716149+appleicat@users.noreply.github.com>
2024-03-27 17:20:32 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
               };
             }
           )
-          ( ./hardware-configuration.nix )
+          ./hardware-configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
