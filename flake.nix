2eee3894dacd76b03ba8f6c0cf6cfb93a2d9f884
kitty <87716149+appleicat@users.noreply.github.com>
2024-03-31 00:50:25 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,11 @@
           ( { config, lib, pkgs, ... }: {
               boot.loader.systemd-boot.enable = true;
               boot.loader.efi.canTouchEfiVariables = true;
+              boot.plymouth.enable = true;
+              boot.consoleLogLevel = 0;
+              boot.initrd.verbose = false;
+              boot.kernelParams = [ "quiet" "udev.log_level=3" ];
+              boot.loader.timeout = 0;
               networking.hostName = "nixos";
               networking.networkmanager.enable = true;
               hardware.opengl.enable = true;
