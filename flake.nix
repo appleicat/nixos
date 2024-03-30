b7f6c6bfcf04b04e5d9b4483e26c37fb05fcb1c7
kitty <87716149+appleicat@users.noreply.github.com>
2024-03-30 16:18:37 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -48,15 +48,72 @@
                 programs.btop = {
                   enable = true;
                 };
+                programs.htop = {
+                  enable = true;
+                };
                 programs.kitty = {
                   enable = true;
                 };
                 programs.waybar = {
                   enable = true;
                 };
+                programs.chromium = {
+                  enable = true;
+                };
                 wayland.windowManager.hyprland = {
                   enable = true;
-                  xwayland.enable = true;
+                  settings = {
+                    "exec-once" = "waybar";
+                    misc = {
+                      "disable_hyprland_logo" = "true";
+                    };
+                    dwindle = {
+                      "no_gaps_when_only" = "1";
+                    };
+                    input = {
+                      touchpad = {
+                        "natural_scroll" = "yes";
+                      };
+                    };
+                    general = {
+                      "border_size" = "3";
+                      "gaps_in" = "3";
+                      "gaps_out" = "5";
+                      "col.active_border" = "rgba(FFFFFFFF)";
+                      "col.inactive_border" = "rgba(FFFFFF00)";
+                    };
+                    "$MOD" = "SUPER";
+                    "$TERM" = "kitty";
+                    bind = [
+                      "$MOD, RETURN, exec, $TERM"
+                      
+                      "$MOD+CTRL, B, exec, $TERM btop"
+                      "$MOD+CTRL, H, exec, $TERM htop"
+                      
+                      "$MOD, C, exec, chromium"
+                      
+                      "$MOD+SHIFT, Q, killactive,"
+                      "$MOD+SHIFT, SPACE, togglefloating,"
+                      "$MOD+SHIFT, F, fullscreen,"
+                      
+                      "$MOD, UP, movefocus, u"
+                      "$MOD, DOWN, movefocus, d"
+                      "$MOD, LEFT, movefocus, l"
+                      "$MOD, RIGHT, movefocus, r"
+                      
+                      "$MOD+CTRL, LEFT, workspace, -1"
+                      "$MOD+CTRL, RIGHT, workspace, +1"
+                      
+                      "$MOD+SHIFT+CTRL, LEFT, movetoworkspace, -1"
+                      "$MOD+SHIFT+CTRL, RIGHT, movetoworkspace, +1"
+                      
+                      "$MOD+SHIFT+CTRL, Q, exit,"
+                    ];
+                    bindm = [
+                      "$MOD, mouse:272, movewindow"
+                      "$MOD, mouse:273, resizewindow"
+                    ];
+                  };
                 };
               }
             );
