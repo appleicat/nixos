9fd5d37dc7a76478d77f96b75434b48c1c97eea7
kitty <87716149+appleicat@users.noreply.github.com>
2024-03-31 19:01:36 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -20,13 +20,20 @@
               networking.hostName = "nixos";
               networking.networkmanager.enable = true;
               hardware.opengl.enable = true;
+              services.pipewire = {
+                enable = true;
+                alsa.enable = true;
+                alsa.support32Bit = true;
+                pulse.enable = true;
+                jack.enable = true;
+              };
               nixpkgs.config.allowUnfree = true;
               nix.settings.experimental-features = [ "nix-command" "flakes" ];
               environment.systemPackages = with pkgs; [ git micro ];
               programs.zsh.enable = true;
               users.users.kitty = {
                 isNormalUser = true;
-                extraGroups = [ "wheel" "networkmanager" ];
+                extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
                 shell = pkgs.zsh;
               };
             }
@@ -43,6 +50,7 @@
                 home.packages = with pkgs; [
                   powerline
                   iosevka
+                  brightnessctl
                 ];
                 programs.zsh = {
                   enable = true;
@@ -69,40 +77,125 @@
                     main = {
                       layer = "top";
                       position = "top";
-                      height = 30;
+                      height = 32;
                       spacing = 5;
                       modules-left = [
                         "hyprland/workspaces"
                       ];
-                      modules-center = [
-                        "clock"
-                      ];
+                      modules-center = [];
                       modules-right = [
+                        "network"
+                        "wireplumber"
+                        "backlight"
+                        "temperature"
                         "disk"
                         "memory"
                         "cpu"
-                        "backlight"
                         "battery"
+                        "clock"
+                        "privacy"
                       ];
+                      "clock" = {
+                        "interval" = 1;
+                        "format-alt" = "{:%A %d %B %Y %H:%M:%S}";
+                        "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+                      };
+                      "battery" = {
+                        "format" = "BAT {capacity}";
+                      };
+                      "cpu" = {
+                        "interval" = 1;
+                        "format" = "CPU {usage}";
+                      };
+                      "memory" = {
+                        "interval" = 10;
+                        "format" = "MEM {percentage}";
+                      };
+                      "temperature" = {
+                        "interval" = 10;
+                        "format" = "TEMP {temperatureC}";
+                      };
+                      "network" = {
+                        "format" = "{ifname}";
+                        "format-wifi" = "{essid}";
+                        "format-ethernet" = "{ipaddr}/{cidr}";
+                        "format-disconnected" = "NO NETWORK";
+                        "tooltip-format" = "{ifname} via {gwaddr}";
+                        "tooltip-format-wifi" = "{essid}\nStrength: {signalStrength}%\nSignal: {signaldBm}dBm";
+                        "tooltip-format-ethernet" = "{ifname}";
+                        "tooltip-disconnected" = "DISCONNECTED";
+                      };
+                      "backlight" = {
+                        "format" = "LIGHT {percent}";
+                      };
+                      "wireplumber" = {
+                        "format" = "AUDIO {volume}";
+                      };
+                      "disk" = {
+                        "format" = "DISK {percentage_used}";
+                      };
+                      "hyprland/workspaces" = {
+                        "format" = "[ {windows} ]";
+                        "format-window-separator" = " | ";
+                        "window-rewrite" = {
+                          "class<kitty> title" = "{title}";
+                          "class" = "{class}";
+                        };
+                      };
                     };
                   };
                   style = ''
                     * {
                       background-color: transparent;
-                      color: white;
                       border: none;
                       border-radius: 0;
                       font-family: Iosevka;
                     }
                     window#waybar {
                       background-color: black;
-                      color: white;
                     }
                     tooltip {
-                      background-color: black;
+                      background-color: white;
                     }
                     tooltip label {
+                      color: black;
+                      font-weight: 300;
+                    }
+                    .modules-left, .modules-center, .modules-right {
+                      margin: 0 5px;
+                    }
+                    #clock,
+                    #battery,
+                    #cpu,
+                    #memory,
+                    #temperature,
+                    #network,
+                    #backlight,
+                    #wireplumber,
+                    #disk,
+                    #privacy, {
+                      background-color: white;
+                      color: black;
+                      padding: 0 10px;
+                    }
+                    #workspaces {
+                      background-color: black;
+                    }
+                    #workspaces button {
+                      background-color: black;
                       color: white;
+                    }
+                    #workspaces button:hover {
+                      background-color: white;
+                      color: black;
+                    }
+                    #workspaces button.active {
+                      background-color: white;
+                      color: black;
+                    }
+                    #workspaces button.empty {
+                      background-color: black;
+                      color: rgba(255,255,255,0);
                     }
                   '';
                 };
@@ -115,9 +208,11 @@
                     "exec-once" = "waybar";
                     misc = {
                       "disable_hyprland_logo" = "true";
+                      "force_default_wallpaper" = "0";
                     };
                     dwindle = {
                       "no_gaps_when_only" = "1";
+                      "smart_split" = "true";
                     };
                     input = {
                       touchpad = {
@@ -132,7 +227,6 @@
                       "col.active_border" = "rgba(FFFFFFFF)";
                       "col.inactive_border" = "rgba(FFFFFF00)";
                       "no_cursor_warps" = "true";
-                      "no_border_on_floating" = "true";
                     };
                     "$MOD" = "SUPER";
                     "$TERM" = "kitty";
@@ -144,10 +238,12 @@
                       
                       "$MOD, C, exec, chromium"
                       
+                      "$MOD, Q, killactive"
                       "$MOD+SHIFT, Q, killactive"
                       "$MOD+SHIFT, SPACE, togglefloating"
                       "$MOD+SHIFT, P, pin"
                       "$MOD+SHIFT, F, fullscreen"
+                      "$MOD+ALT, S, togglesplit"
                       
                       "$MOD, UP, movefocus, u"
                       "$MOD, DOWN, movefocus, d"
@@ -172,6 +268,18 @@
                       "ALT, TAB, cyclenext"
                       
                       "$MOD+SHIFT+CTRL, Q, exit"
+                    ];
+                    binde = [
+                      "$MOD+ALT, UP, splitratio, -0.1"
+                      "$MOD+ALT, LEFT, splitratio, -0.1"
+                      "$MOD+ALT, DOWN, splitratio, 0.1"
+                      "$MOD+ALT, RIGHT, splitratio, 0.1"
+                    ];
+                    bindle = [
+                      ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+                      ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+                      ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+                      ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-"
                     ];
                     bindm = [
                       "$MOD, mouse:272, movewindow"
