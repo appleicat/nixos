d0715c512bc9826f7d929416ea811b6db049d007
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-03 01:26:39 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -176,6 +176,40 @@
                           background = "#A6ADC8";
                         };
                       };
+                      selection = {
+                        text = "#1E1E2E";
+                        background = "#F5E0DC";
+                      };
+                      normal = {
+                        black = "#45475A";
+                        red = "#F38BA8";
+                        green = "#A6E3A1";
+                        yellow = "#F9E2AF";
+                        blue = "#89B4FA";
+                        magenta = "#F5C2E7";
+                        cyan = "#94E2D5";
+                        white = "#BAC2DE";
+                      };
+                      bright = {
+                        black = "#585B70";
+                        red = "#F38BA8";
+                        green = "#A6E3A1";
+                        yellow = "#F9E2AF";
+                        blue = "#89B4FA";
+                        magenta = "#F5C2E7";
+                        cyan = "#94E2D5";
+                        white = "#A6ADC8";
+                      };
+                      dim = {
+                        black = "#45475A";
+                        red = "#F38BA8";
+                        green = "#A6E3A1";
+                        yellow = "#F9E2AF";
+                        blue = "#89B4FA";
+                        magenta = "#F5C2E7";
+                        cyan = "#94E2D5";
+                        white = "#BAC2DE";
+                      };
                     };
                     cursor = {
                       style = {
