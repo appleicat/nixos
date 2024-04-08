a0f43d0b206a3626b409012837448838d1ee78d7
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-08 20:00:23 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -98,6 +98,7 @@
                 home.pointerCursor = {
                   package = pkgs.apple-cursor;
                   name = "macOS-Monterey-White";
+                  size = 16;
                   gtk.enable = true;
                   x11.enable = true;
                 };
