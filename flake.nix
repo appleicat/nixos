59b448c8c35921a773624ff3ddc54619d83e9b83
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-10 22:59:18 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -16,6 +16,44 @@
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
+          {
+            disko = {
+              devices = {
+                disk = {
+                  main = {
+                    type = "disk";
+                    device = "";
+                    content = {
+                      type = "gpt";
+                      partitions = {
+                        ESP = {
+                          priority = 1;
+                          name = "ESP";
+                          start = "1M";
+                          end = "512M";
+                          type = "EF00";
+                          content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                          };
+                        };
+                        root = {
+                          size = "100%";
+                          content = {
+                            type = "btrfs";
+                            extraArgs = [ "-f" ];
+                            mountpoint = "/";
+                            mountOptions = [ "compress=zstd" "noatime" ];
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          }
           ( { config, lib, pkgs, ... }: {
               boot.loader.systemd-boot.enable = true;
               boot.loader.efi.canTouchEfiVariables = true;
