5abe2820848b12eb66ec11cbc6367683a5573bd5
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-11 15:04:21 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,55 +5,12 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { nixpkgs, home-manager, disko, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          disko.nixosModules.disko
-          {
-            disko = {
-              devices = {
-                disk = {
-                  main = {
-                    type = "disk";
-                    device = "";
-                    content = {
-                      type = "gpt";
-                      partitions = {
-                        ESP = {
-                          priority = 1;
-                          name = "ESP";
-                          start = "1M";
-                          end = "512M";
-                          type = "EF00";
-                          content = {
-                            type = "filesystem";
-                            format = "vfat";
-                            mountpoint = "/boot";
-                          };
-                        };
-                        root = {
-                          size = "100%";
-                          content = {
-                            type = "btrfs";
-                            extraArgs = [ "-f" ];
-                            mountpoint = "/";
-                            mountOptions = [ "compress=zstd" "noatime" ];
-                          };
-                        };
-                      };
-                    };
-                  };
-                };
-              };
-            };
-          }
           ( { config, lib, pkgs, ... }: {
               boot.loader.systemd-boot.enable = true;
               boot.loader.efi.canTouchEfiVariables = true;
