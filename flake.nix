67b7c4fcc4e3ddcd00121ef3421939742d4265dc
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-12 23:39:02 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,52 +18,7 @@
       "${hostname}" = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          (
-            { pkgs, ... }: {
-              boot.loader.systemd-boot.enable = true;
-              boot.loader.efi.canTouchEfiVariables = true;
-             #boot.plymouth.enable = true;
-             #boot.consoleLogLevel = 0;
-             #boot.initrd.verbose = false;
-             #boot.kernelParams = [ "quiet" "udev.log_level=3" ];
-              boot.loader.timeout = 0;
-              networking.hostName = "${hostname}";
-              networking.wireless.iwd.enable = true;
-              networking.networkmanager.enable = true;
-              networking.networkmanager.wifi.backend = "iwd";
-              hardware.opengl = {
-                enable = true;
-                driSupport = true;
-                driSupport32Bit = true;
-              };
-              sound.enable = true;
-              services.pipewire = {
-                enable = true;
-                alsa.enable = true;
-                alsa.support32Bit = true;
-                pulse.enable = true;
-                jack.enable = true;
-              };
-              powerManagement = {
-                enable = true;
-                powertop.enable = true;
-              };
-              i18n = {
-                defaultLocale = "en_US.UTF-8";
-                supportedLocales = [ "all" ];
-              };
-              nixpkgs.config.allowUnfree = true;
-              nix.settings.experimental-features = [ "nix-command" "flakes" ];
-              environment.systemPackages = with pkgs; [ git micro ];
-              programs.zsh.enable = true;
-              users.users."${username}" = {
-                isNormalUser = true;
-                extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
-                shell = pkgs.zsh;
-              };
-              system.stateVersion = "${stateVersion}";
-            }
-          )
+          ( import ./system/configuration.nix { inherit hostname username stateVersion; } )
           ./hardware-configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
