147591378ad1a48d83f0ff14df6158c3bf860788
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-13 23:25:34 +0400

    Update apps.nix

--- home-manager/apps.nix
+++ home-manager/apps.nix
@@ -1,6 +1,29 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 {
   home.packages = with pkgs; [
-    
+    brightnessctl
+   #cmatrix
+   #neofetch
+   #tty-clock
+    swww
+   #mpvpaper
+   #bun
+   #deno
+   #nodejs
+   #warp-terminal
+   #hyper
+   #discord
+   #vesktop
+   #cmus
+   #ffmpeg-full
+   #fim
+   #timg
+   #catimg
+   #grim
+   #slurp
+   #wf-recorder
+   #qemu
+   #retroarchFull
+    mc
   ];
 }
