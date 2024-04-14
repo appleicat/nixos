4f138c2e1cac277eab0b8462535bc6b977a9a800
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-14 17:15:04 +0400

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -1,13 +1,22 @@
+{ silent ? false }:
+{ lib, ... }:
 {
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      timeout = 0;
-    };
-   #plymouth.enable = true;
-   #consoleLogLevel = 0;
-   #initrd.verbose = false;
-   #kernelParams = [ "quiet" "udev.log_level=3" ];
-  };
+  boot = lib.mkMerge
+  [
+    {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+        timeout = 0;
+      };
+    }
+    (
+      lib.mkIf silent {
+        plymouth.enable = true;
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        kernelParams = [ "quiet" "udev.log_level=3" ];
+      }
+    )
+  ];
 }
