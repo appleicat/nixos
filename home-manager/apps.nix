bbc628db5d4ff21262892dd188985640634f98fc
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-14 13:26:48 +0400

    Update apps.nix

--- home-manager/apps.nix
+++ home-manager/apps.nix
@@ -26,4 +26,70 @@
    #retroarchFull
     mc
   ];
+  programs.btop = {
+    enable = true;
+  };
+  programs.htop = {
+    enable = true;
+  };
+  programs.tmux = {
+    enable = true;
+  };
+  programs.eza = {
+    enable = true;
+  };
+ #programs.wezterm = {
+ #  enable = true;
+ #};
+ #programs.foot = {
+ #  enable = true;
+ #};
+  programs.ranger = {
+    enable = true;
+  };
+  programs.lf = {
+    enable = true;
+  };
+  programs.yazi = {
+    enable = true;
+  };
+  programs.bat = {
+    enable = true;
+  };
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "monokai-dark";
+      infobar = false;
+      statusline = true;
+      tabstospaces = true;
+      tabsize = 2;
+      ruler = true;
+    };
+  };
+  programs.helix = {
+    enable = true;
+  };
+ #programs.vscode = {
+ #  enable = true;
+ #};
+ #services.mpd = { enable = true; };
+ #programs.ncmpcpp = {
+ #  enable = true;
+ #};
+ #programs.mpv = {
+ #  enable = true;
+ #};
+  programs.bemenu = {
+    enable = true;
+  };
+ #programs.chromium = {
+ #  enable = true;
+ #};
+ #programs.firefox = {
+ #  enable = true;
+ #};
+ #services.mako = {
+ #  enable = true;
+ #};
 }
