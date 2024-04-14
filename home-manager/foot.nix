b8e99554cccd04b3c6bc64266f9d1c0f3db1e9c9
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-14 19:26:42 +0400

    Update foot.nix

--- home-manager/foot.nix
+++ home-manager/foot.nix
@@ -1,5 +1,16 @@
 {
   programs.foot = {
     enable = true;
+    settings = {
+      main = {
+        font = "Iosevka:size=13";
+        pad = "15x15";
+      };
+      colors = {
+        alpha = 1.0;
+        background = "000000";
+        foreground = "FFFFFF";
+      };
+    };
   };
 }
