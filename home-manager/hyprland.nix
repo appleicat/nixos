0b544f4d7f17c8ec85135fd61a814a723203e3f0
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-14 16:27:52 +0400

    Update hyprland.nix

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -131,8 +131,8 @@
         "$MOD+ALT, RIGHT, moveactive, 10 0"
       ];
       bindle = [
-        ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86MonBrightnessUp, exec, brillo -u 100000 -A 10"
+        ", XF86MonBrightnessDown, exec, brillo -u 100000 -U 10"
         ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
         ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-"
       ];
